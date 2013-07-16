282d935916ee9d7b99abbce5046f17e1425e1c08
Spencer Reynoso <spencerreynoso@live.com>
2013-07-15 19:29:34 -0500

    zip working for list and tuples but not sets

--- ziptest.py
+++ ziptest.py
@@ -17,25 +17,29 @@ assert(zip(zip([1, 2, 3]), [0, 0 ,0]) == [((1,), 0),((2,), 0), ((3,), 0)])
 
 def my_zip (*args):
   a = []
+  if len(args) == 0:
+      return a
   size = min(len(temp) for temp in args)
   if size == 0 :
    return a
 
-  elif len(args) == 1 : 
-    for it in args:
-      for tup in it:
-        a.append((tup, ))
-      return a
   else :
-    temp = () 
-    for it in args:
-      for tup in it:
-        temp += (tup,)
-        break
-      a.append(temp)
+    i = 0
+    t = 0
+    while t < size:
+        temp = ()
+        while i < len(args) :
+            temp += (args[i][t],)
+            i += 1
+        t += 1
+        i = 0
+        a.append(temp)
     return a
-  
-print my_zip((), ())
-print my_zip([1,2,3])
-print my_zip([1,2,3], [1,2,3])
 
+
+assert zip((), ()) == my_zip((),())
+assert zip([1, 2, 3]) == my_zip([1,2,3])
+assert zip((1, 2, 3), [1, 2, 3]) == my_zip((1, 2, 3), [1, 2, 3])
+assert zip(()) == my_zip(())
+assert zip() == my_zip()
+print zip(set([1, 2, 3]))
